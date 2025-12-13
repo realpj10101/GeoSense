50294eee95d54b0847ceb50d854d37dc9f68af87
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-13 17:32:13 +0330

    Made point dto to record

--- api/Models/Shape.cs
+++ api/Models/Shape.cs
@@ -1,10 +1,9 @@
 namespace api.Models;
 
-public class PointDto
-{
-    public double X { get; set; }
-    public double Y { get; set; }
-}
+public record PointDto(
+    double X,
+    double Y
+);
 
 public class ShapeRequest
 {
