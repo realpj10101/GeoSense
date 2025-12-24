5215f27782c69831f6b6a1d9db8ca821fe69d19c
realpj10101 <parsajfr1385oo@gmail.com>
2025-12-24 14:18:10 +0330

    Created analyze controller

--- api/Controllers/ShapeAnalysisController.cs
+++ api/Controllers/ShapeAnalysisController.cs
@@ -1,15 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using api.Enums;
+using api.Helpers;
+using api.Interfaces;
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace api.Controllers
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShapeAnalysisController(IShapeAnalysisService _shapeAnalysisService) : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ShapeAnalysisController : ControllerBase
+    [HttpPost]
+    public ActionResult<ShapeResponse> Analyze(ShapeRequest req)
     {
-        
+        OperationResult<ShapeResponse> opResult = _shapeAnalysisService.Analyze(req);
+
+        return opResult.IsSuccess
+        ? opResult.Result
+        : opResult.Error?.Code switch
+        {
+            ErrorCode.IsNotFourPoints => BadRequest(opResult.Error.Message),
+            ErrorCode.ArePointsSame => BadRequest(opResult.Error.Message),
+            ErrorCode.AreSidesApprove => BadRequest(opResult.Error.Message),
+            ErrorCode.IsRhombus => BadRequest(opResult.Error.Message),
+            _ => BadRequest("Something went wrong")
+        };
     }
 }
